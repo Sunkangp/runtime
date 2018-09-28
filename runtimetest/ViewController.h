b15572bcdcded40f1508ebfd5d47fa037bd94b80
sunkang <958607177@qq.com>
2018-09-28 17:05:37 +0800

    Initial Commit

---
+++ runtimetest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  runtimetest
+//
+//  Created by sunkang on 2018/9/28.
+//  Copyright © 2018年 sunkang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ runtimetest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  runtimetest
+//
+//  Created by sunkang on 2018/9/28.
+//  Copyright © 2018年 sunkang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
